39d05396d7d76f5c49c3359d1a339e023e065cbf
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-07 14:33:47 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -8,4 +8,9 @@ class Captain < ActiveRecord::Base
   def self.sailors
     self.joins(:boats).joins(boat_classifications: :classifications).where("classifications.name = 'sailboat'")
   end
+
+  def talented_seafarers
+    self.self.joins(:boats).joins(boat_classifications: :classifications).where("classifications.name IN ('sailboat', ' motorboat')")
+  end
+  
 end
