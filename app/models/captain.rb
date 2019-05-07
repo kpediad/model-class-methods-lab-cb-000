8641cd632d8096955984124c1747dd070904b412
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-07 15:28:22 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -3,19 +3,19 @@ class Captain < ActiveRecord::Base
 
   def self.catamaran_operators
     binding.pry
-    self.joins(:boats).joins(boat_classifications: :classification).where("classifications.name = 'Catamaran'")
+    self.joins(boats: {boat_classifications: :classification}).where("classifications.name = 'Catamaran'")
   end
 
   def self.sailors
-    self.joins(:boats).joins(boat_classifications: :classification).where("classifications.name = 'Sailboat'")
+    self.joins(boats: {boat_classifications: :classification}).where("classifications.name = 'Sailboat'")
   end
 
   def self.talented_seafarers
-    self.self.joins(:boats).joins(boat_classifications: :classification).where("classifications.name IN ('Sailboat', ' Motorboat')")
+    self.joins(boats: {boat_classifications: :classification}).where("classifications.name IN ('Sailboat', ' Motorboat')")
   end
 
   def self.non_sailors
-    self.joins(:boats).joins(boat_classifications: :classification).where.not("classifications.name = 'Sailboat'")
+    self.joins(boats: {boat_classifications: :classification}).where.not("classifications.name = 'Sailboat'")
   end
 
 end
