52628c749818b80ac64c2153d16c7abd18891279
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-07 19:24:22 +0000

    Automatically backed up by Learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -7,7 +7,7 @@ class Classification < ActiveRecord::Base
   end
 
   def self.longest
-    binding.pry
-    self.joins(boat_classifications: :boat).order("boats.length desc").limit(1)
+    longest_boat_id = self.joins(boat_classifications: :boat).order("boats.length desc").select("boats.id").pluck("boats.id").first
+    self.joins(:boat_classifications).where("boat_classifications.boat_id = ?", longest_boat_id)
   end
 end
