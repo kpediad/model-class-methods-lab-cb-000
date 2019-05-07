b5ec49ac167f6b2dcb64722062e7e491379e8999
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-07 18:10:51 +0000

    Automatically backed up by Learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -7,6 +7,6 @@ class Classification < ActiveRecord::Base
   end
 
   def self.longest
-    self.joins(boat_classifications: :boat).order(length: :desc).first
+    self.joins(boat_classifications: :boat).order("boats.length desc").first
   end
 end
